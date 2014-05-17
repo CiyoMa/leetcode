46f6dbddfd62f0573ebce1e4f6a4ae7a18fb8514
CiyoMa <ciyo.ma@gmail.com>
2014-05-16 21:04:15 -0400

    May 16

--- isBalanced.py
+++ isBalanced.py
@@ -9,4 +9,12 @@ class Solution:
     # @param root, a tree node
     # @return a boolean
     def isBalanced(self, root):
-        
+        return root == None or \
+               self.isBalanced(root.left) and \
+               self.isBalanced(root.right) and \
+               abs(self.height(root.left) - self.height(root.right)) <= 1
+
+    def height(self, root):
+    	if root is None:
+    		return 0
+    	return max(self.height(root.left), self.height(root.right)) + 1
--- mergeSortedArray.py
+++ mergeSortedArray.py
@@ -5,4 +5,20 @@ class Solution:
     # @param n  an integer, length of B
     # @return nothing
     def merge(self, A, m, B, n):
-        
+        a = m - 1
+        b = n - 1
+        l = m + n - 1
+        while a >= 0 and b >= 0:
+        	if A[a] > B[b]:
+        		A[l] = A[a]
+        		a -= 1
+        	else:
+        		A[l] = B[b]
+        		b -= 1
+        	l -= 1
+        while b>= 0 :
+        	A[l] =B[b]
+        	l -= 1
+        	b -= 1
+
+
