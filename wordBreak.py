857787c6c8ef665b03b5d2fbb226dbfa6183ad89
CiyoMa <ciyo.ma@gmail.com>
2014-05-22 23:20:29 -0400

    May 22

--- wordBreak.py
+++ wordBreak.py
@@ -10,64 +10,93 @@ dict = ["leet", "code"].
 Return true because "leetcode" can be segmented as "leet code".
 """
 
+class Solution:
+    # @param s, a string
+    # @param dict, a set of string
+    # @return a list of strings
+    def wordBreak(self, s, dict):
+        dp = [[] if i > 0 else ['dummy'] for i in range(len(s)+1)]
+
+        for i in range(1,len(s)+1):
+            for j in range(i):
+                #print s[j:i]
+                if len(dp[j]) and s[j:i] in dict:
+                    dp[i].append(j)
+        #print dp
+
+        return len(dp[len(s)]) > 0
+
+# stack + DP version, not just naive 1 step dfs, but still some redundency
+# "abcd", ['def','gf']
+# TO-DO: try just DP
 # class Solution:
 #     # @param s, a string
 #     # @param dict, a set of string
 #     # @return a boolean
 #     def wordBreak(self, s, dict):
 
+#         stack = [0]
+#         n = len(s)
+#         #print n
+#         i = 1
+#         last = 0
+#         valid = [False for j in range(n)]
+#         flag = False
+#         farthest = 0
+#         while farthest < n:
+#             #print valid
+#             #print i == n+1, i, n-1, valid[n-1]
+#             if i == n+1 and valid[i-2]:
+#                 return True
+            
+#             if valid[i-1]:
+#                 i += 1
+#                 continue
+#             #print stack, 
+#             temp = last #0 if last == 1 else last
+#             if s[last:i] in dict:
+#                 flag = True
+#                 stack.append(i)
+#                 last = i
+#                 valid[i-1] = True
 
-# stack + DP version, not just naive 1 step dfs, 
-# TO-DO: try just DP
-class Solution:
-    # @param s, a string
-    # @param dict, a set of string
-    # @return a boolean
-    def wordBreak(self, s, dict):
-        stack = [0]
-        n = len(s)
-        #print n
-        i = 1
-        last = 0
-        valid = [False for j in range(n)]
-        while i <= n:
-        	if valid[i-1]:
-        		i += 1
-        		continue
-        	#print stack, 
-        	temp = last #0 if last == 1 else last
-        	if s[last:i] in dict:
-        		flag = True
-        		stack.append(i)
-        		last = i
-        		valid[i-1] = True
+#             #print temp, i, flag,farthest, valid
+#             flag = False
+#             if i == n and last != n:# and len(stack) > 0:
+#                 #last = stack.pop()
+#                 i = last
+#                 last = stack.pop()
+#                 if len(stack) == 0:
+#                     stack.append(0)
+#                     farthest += 1
 
-        	#print temp, i, flag
-        	flag = False
-        	if i == n and last != n and len(stack) > 0:
-        		#last = stack.pop()
-        		i = last
-        		last = stack.pop()
-        		# if len(stack) > 0:
-        		# 	last = stack.pop()
-        		# else:
-        		# 	last = 0
-        		# while i < n-1 and valid[i+1]:
-        		# 	i += 1
-        	elif i == n and last != n:
-        		return False
 
-        	i += 1
+#                 # if len(stack) > 0:
+#                 #     last = stack.pop()
+#                 # else:
+#                 #     last = 0
+#                 # while i < n-1 and valid[i+1]:
+#                 #     i += 1
+#             # elif i == n and last != n:
+#             #     #print '!'
+#             #     #last = 0
+#             #     #i = 1
+#             #     stack.append(0)
+#                 #return False
 
-        if stack.pop() == n:
-        	return True
+#             i += 1
 
-        return False
+#         # if stack.pop() == n and n != 0:
+#         #     return True
+
+#         return False
 
 st,dic = "fohhemkkaecojceoaejkkoedkofhmohkcjmkggcmnami", ["kfomka","hecagbngambii","anobmnikj","c","nnkmfelneemfgcl","ah","bgomgohl","lcbjbg","ebjfoiddndih","hjknoamjbfhckb","eioldlijmmla","nbekmcnakif","fgahmihodolmhbi","gnjfe","hk","b","jbfgm","ecojceoaejkkoed","cemodhmbcmgl","j","gdcnjj","kolaijoicbc","liibjjcini","lmbenj","eklingemgdjncaa","m","hkh","fblb","fk","nnfkfanaga","eldjml","iejn","gbmjfdooeeko","jafogijka","ngnfggojmhclkjd","bfagnfclg","imkeobcdidiifbm","ogeo","gicjog","cjnibenelm","ogoloc","edciifkaff","kbeeg","nebn","jdd","aeojhclmdn","dilbhl","dkk","bgmck","ohgkefkadonafg","labem","fheoglj","gkcanacfjfhogjc","eglkcddd","lelelihakeh","hhjijfiodfi","enehbibnhfjd","gkm","ggj","ag","hhhjogk","lllicdhihn","goakjjnk","lhbn","fhheedadamlnedh","bin","cl","ggjljjjf","fdcdaobhlhgj","nijlf","i","gaemagobjfc","dg","g","jhlelodgeekj","hcimohlni","fdoiohikhacgb","k","doiaigclm","bdfaoncbhfkdbjd","f","jaikbciac","cjgadmfoodmba","molokllh","gfkngeebnggo","lahd","n","ehfngoc","lejfcee","kofhmoh","cgda","de","kljnicikjeh","edomdbibhif","jehdkgmmofihdi","hifcjkloebel","gcghgbemjege","kobhhefbbb","aaikgaolhllhlm","akg","kmmikgkhnn","dnamfhaf","mjhj","ifadcgmgjaa","acnjehgkflgkd","bjj","maihjn","ojakklhl","ign","jhd","kndkhbebgh","amljjfeahcdlfdg","fnboolobch","gcclgcoaojc","kfokbbkllmcd","fec","dljma","noa","cfjie","fohhemkka","bfaldajf","nbk","kmbnjoalnhki","ccieabbnlhbjmj","nmacelialookal","hdlefnbmgklo","bfbblofk","doohocnadd","klmed","e","hkkcmbljlojkghm","jjiadlgf","ogadjhambjikce","bglghjndlk","gackokkbhj","oofohdogb","leiolllnjj","edekdnibja","gjhglilocif","ccfnfjalchc","gl","ihee","cfgccdmecem","mdmcdgjelhgk","laboglchdhbk","ajmiim","cebhalkngloae","hgohednmkahdi","ddiecjnkmgbbei","ajaengmcdlbk","kgg","ndchkjdn","heklaamafiomea","ehg","imelcifnhkae","hcgadilb","elndjcodnhcc","nkjd","gjnfkogkjeobo","eolega","lm","jddfkfbbbhia","cddmfeckheeo","bfnmaalmjdb","fbcg","ko","mojfj","kk","bbljjnnikdhg","l","calbc","mkekn","ejlhdk","hkebdiebecf","emhelbbda","mlba","ckjmih","odfacclfl","lgfjjbgookmnoe","begnkogf","gakojeblk","bfflcmdko","cfdclljcg","ho","fo","acmi","oemknmffgcio","mlkhk","kfhkndmdojhidg","ckfcibmnikn","dgoecamdliaeeoa","ocealkbbec","kbmmihb","ncikad","hi","nccjbnldneijc","hgiccigeehmdl","dlfmjhmioa","kmff","gfhkd","okiamg","ekdbamm","fc","neg","cfmo","ccgahikbbl","khhoc","elbg","cbghbacjbfm","jkagbmfgemjfg","ijceidhhajmja","imibemhdg","ja","idkfd","ndogdkjjkf","fhic","ooajkki","fdnjhh","ba","jdlnidngkfffbmi","jddjfnnjoidcnm","kghljjikbacd","idllbbn","d","mgkajbnjedeiee","fbllleanknmoomb","lom","kofjmmjm","mcdlbglonin","gcnboanh","fggii","fdkbmic","bbiln","cdjcjhonjgiagkb","kooenbeoongcle","cecnlfbaanckdkj","fejlmog","fanekdneoaammb","maojbcegdamn","bcmanmjdeabdo","amloj","adgoej","jh","fhf","cogdljlgek","o","joeiajlioggj","oncal","lbgg","elainnbffk","hbdi","femcanllndoh","ke","hmib","nagfahhljh","ibifdlfeechcbal","knec","oegfcghlgalcnno","abiefmjldmln","mlfglgni","jkofhjeb","ifjbneblfldjel","nahhcimkjhjgb","cdgkbn","nnklfbeecgedie","gmllmjbodhgllc","hogollongjo","fmoinacebll","fkngbganmh","jgdblmhlmfij","fkkdjknahamcfb","aieakdokibj","hddlcdiailhd","iajhmg","jenocgo","embdib","dghbmljjogka","bahcggjgmlf","fb","jldkcfom","mfi","kdkke","odhbl","jin","kcjmkggcmnami","kofig","bid","ohnohi","fcbojdgoaoa","dj","ifkbmbod","dhdedohlghk","nmkeakohicfdjf","ahbifnnoaldgbj","egldeibiinoac","iehfhjjjmil","bmeimi","ombngooicknel","lfdkngobmik","ifjcjkfnmgjcnmi","fmf","aoeaa","an","ffgddcjblehhggo","hijfdcchdilcl","hacbaamkhblnkk","najefebghcbkjfl","hcnnlogjfmmjcma","njgcogemlnohl","ihejh","ej","ofn","ggcklj","omah","hg","obk","giig","cklna","lihaiollfnem","ionlnlhjckf","cfdlijnmgjoebl","dloehimen","acggkacahfhkdne","iecd","gn","odgbnalk","ahfhcd","dghlag","bchfe","dldblmnbifnmlo","cffhbijal","dbddifnojfibha","mhh","cjjol","fed","bhcnf","ciiibbedklnnk","ikniooicmm","ejf","ammeennkcdgbjco","jmhmd","cek","bjbhcmda","kfjmhbf","chjmmnea","ifccifn","naedmco","iohchafbega","kjejfhbco","anlhhhhg"]
 #st,dic ="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
 #st,dic = "leetecocode", ["leet", "leete","ec", "eco", "code"]
-#st,dic = "ab", ["a","b"]
+st,dic = "ab", ["a","b"]
+st, dic ="a", []#["a"]
+#st,dic = "cabceb", ['b','ab','abc','abce']
 s = Solution()
 print s.wordBreak(st, dic)
 
---
+++ wordBreakII.py
@@ -0,0 +1,95 @@
+"""
+Given a string s and a dictionary of words dict, add spaces 
+in s to construct a sentence where each word is a valid dictionary word.
+
+Return all such possible sentences.
+
+For example, given
+s = "catsanddog",
+dict = ["cat", "cats", "and", "sand", "dog"].
+
+A solution is ["cats and dog", "cat sand dog"].
+"""
+
+class Solution:
+    # @param s, a string
+    # @param dict, a set of string
+    # @return a list of strings
+    def wordBreak(self, s, dict):
+    	# def convert(dp, end, l):
+    	# 	result = []
+    	# 	for i in dp[end]:
+    	# 		tail = s[i:end]
+    	# 		if i == 0:
+    	# 			result += [tail]
+    	# 			continue
+    	# 		temp = convert(dp, i, l) 
+
+    	# 		for j in range(len(temp)):
+    	# 			temp[j] += ' ' + tail
+    	# 		result += temp
+    	# 	#print end, result
+    	# 	return result
+
+
+    	################################################################
+    	################# SINGLE PASS -- FAIL, For No Result Case, 
+    	################# too many interminate result say, waste memory  
+    	################################################################
+        # dp = [[] if i > 0 else [''] for i in range(len(s)+1)]
+
+        # for i in range(1,len(s)+1):
+        # 	for j in range(i):
+        # 		#print j,i,#s[j:i],
+        # 		tail = s[j:i]
+        # 		if len(dp[j]) and tail in dict:
+        # 			print True
+        # 			if i == len(s):
+        # 				BLANK = ''
+        # 			else:
+        # 				BLANK = ' '
+        # 			for k in dp[j]:
+        # 				dp[i].append(k+tail+BLANK)
+        # 			print dp[i]
+        # 			continue
+        # 		print
+        # #print dp
+        # return dp[len(s)]
+        #################### FULL ############################
+
+        dp = [[] if i > 0 else [''] for i in range(len(s)+1)]
+
+        for i in range(1,len(s)+1):
+        	for j in range(i-1,-1,-1):
+        		#print j,i,s[j:i],
+        		#tail = s[j:i]
+        		if len(dp[j]) and s[j:i] in dict:
+        			#print True
+        			dp[i].append(j)
+        			continue
+        		#print
+        #print dp
+        l = len(s)
+        if len(dp[l]) <= 0: return []
+        resultList = [[] for i in range(l)]
+        for i in range(l):
+        	for j in dp[i+1]:
+        		tail = s[j:i+1]
+        		if i == l-1:
+        			BLANK = ''
+        		else:
+        			BLANK = ' '
+        		if j == 0:
+        			resultList[i].append(tail+BLANK)
+        		else:
+        			for k in resultList[j-1]:
+        				resultList[i].append(k+tail+BLANK)
+        #print resultList
+        return resultList[l-1]
+
+        return convert(dp, l, l)
+
+st,dic = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
+#"bbbbbbbbbbbbb", ['b','bb','abc','abce']
+s = Solution()
+print s.wordBreak(st, dic)
