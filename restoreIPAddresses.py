d578559105571fbd74f186036680236923c17af9
CiyoMa <ciyo.ma@gmail.com>
2014-05-25 21:34:10 -0400

    May 25

--- restoreIPAddresses.py
+++ restoreIPAddresses.py
@@ -12,4 +12,32 @@ class Solution:
     # @param s, a string
     # @return a list of strings
     def restoreIpAddresses(self, s):
-        
+    	def dfs(start, depth):
+    		#print start
+    		if len(s) - start < 1 or len(s) - start > 3 and depth == 3 or len(s) - start > 1 and len(s) - start <= 3 and depth == 3 and s[start] == '0':
+    			return []
+    		if len(s) - start <= 3 and depth == 3 and int(s[start:]) >= 0 and int(s[start:]) <= 255:
+    			return [ s[start:] ]
+    		result = []
+    		for i in range(1,4):
+    			ip = s[start:start+i]
+    			if s[start] == '0' and len(ip) > 1:
+    				break
+    			#print ip, start, i
+    			if len(ip) > 0 and (int(ip) < 0 or int(ip) > 255):
+    				continue
+    			next = start+i
+    			res = dfs(next, depth + 1)
+    			# if res == []:
+    			# 	print res, '!'
+    			# 	continue
+    			for r in res:
+    				result.append(ip + '.' + r)
+    			# print result, '#'
+    		return  result
+    	return dfs(0,0)
+
+ip = "0000" #"25525511135"
+ip = "010010"
+s = Solution()
+print s.restoreIpAddresses(ip)
