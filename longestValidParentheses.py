39368356e63ca3a5e05529411b4a160194d2566f
CiyoMa <ciyo.ma@gmail.com>
2014-06-11 01:45:30 -0400

    June 11

--- longestValidParentheses.py
+++ longestValidParentheses.py
@@ -1,3 +1,13 @@
+"""
+Given a string containing just the characters '(' and ')', 
+find the length of the longest valid (well-formed) parentheses substring.
+
+For "(()", the longest valid parentheses substring is "()", which has length = 2.
+
+Another example is ")()())", where the longest valid parentheses substring is "()()", 
+which has length = 4.
+"""
+
 class Solution:
     # @param s, a string
     # @return an integer
@@ -6,33 +16,33 @@ class Solution:
         longest = 0
         n = len(s)
         i = 0
-        temp = 0
-        count = 0
         while i < n:
-        	char = s[i]
-        	longest = max(longest, temp)
-        	if char in ['(', '[', '{']:
-        		count += 1
-        		stack.append(char)
-        	else:
-        		count -= 1
-        		if len(stack) == 0:
-        			i += 1
-        			# longest = max(longest, temp)
-        			temp = 0
-        			continue
-        		left = stack.pop()
-        		if left == '{' and char == '}' or\
-        		   left == '(' and char == ')' or\
-        		   left == '[' and char == ']':
-        		    temp += 2
-        		    i += 1
-        		    longest = max(temp, longest)
-        		    continue
-        	i += 1
-        return longest#-len(stack)
+            char = s[i]
+            if char == '(':
+                stack.append(i) 
+            else:
+                # no ( matches extra ), continuity breaks
+                if len(stack) == 0:
+                    stack.append(i)
+                    i += 1
+                    continue
+                left = stack.pop()
+
+                if s[left] == '(':
+                    if len(stack) == 0:
+                        temp = i + 1
+                    else:
+                        temp = i - stack[len(stack)-1]
+                        # print temp
+                    longest = max(temp, longest)
+                    i += 1
+                    continue
+                # no ( matches extra ), continuity breaks
+                stack.append(i)
+            i += 1
+        return longest
 
 s = Solution()
-print s.longestValidParentheses("()(()")
+print s.longestValidParentheses("()()))()(())")
 
 
